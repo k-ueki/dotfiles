8e0b29381a9e67ea4d9685004ebbf4b500f97ed9
k-ueki <ku6.gatt@gmail.com>
2023-01-16 11:34:05 +0900

    add chrome-cli for setup

--- setup/setup_opts.sh
+++ setup/setup_opts.sh
@@ -8,3 +8,5 @@ git clone https://github.com/rupa/z $HOME/tools/z
 # setup zsh-abbr
 git clone https://github.com/olets/zsh-abbr.git $HOME/tools/zsh-abbr
 
+# setup OREORE chrome-cli
+git clone https://github.com/k-ueki/chrome-cli.git $HOME/tools/chrome-cli
