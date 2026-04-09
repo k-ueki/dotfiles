4665b19ff80c031b61d9f5b3337de4cf5bfddc04
k-ueki <ku6.gatt@gmail.com>
2026-04-09 11:23:18 +0900

    chore(setup): remove unused setup_apps.sh and setup_dein.sh

--- setup/setup_apps.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-brew update
-
-brew bundle
--- setup/setup_dein.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#dein install
-mkdir -p ~/.cache/dein
-cd ~/.cache/dein
-
-# curl script for installing dein.vim
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-sh ./installer.sh ~/.cache/dein
