7e9ae56d5a5df6487da27b417951ec255267e4b2
k-ueki <ku6.gatt@gmail.com>
2026-04-09 12:37:42 +0900

    chore: replace ~/mybin symlink with direct dotfiles/bin PATH

--- setup/setup_dotfiles.sh
+++ setup/setup_dotfiles.sh
@@ -19,8 +19,6 @@ link $DOT_PATH/zsh/zlogin      ~/.zlogin
 link $DOT_PATH/zsh/zimrc       ~/.zimrc
 link $DOT_PATH/zsh/zim/gitster ~/.zim/modules/gitster
 
-link $DOT_PATH/bin             ~/mybin
-
 link $DOT_PATH/.ideavimrc 	   ~/.ideavimrc
 
 link $DOT_PATH/alacritty 	   ~/.config/alacritty
